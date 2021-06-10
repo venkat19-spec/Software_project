5444a6af47c68376fab24c705fe70835946f57c4
hareesh2904 <hareesh290401@gmail.com>
2021-06-10 18:43:04 +0530

    Update checks.js

--- views/checks.js
+++ views/checks.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 const take =document.querySelector('#take');
 take.onclick = function(){
-    const searchValue = document.querySelector('#semester-input').value;
+    const searchValue = document.querySelector('#name-input').value;
     fetch('http://localhost:5000/getall_check'+ searchValue)
     .then(response => response.json())
     .then(data => loadHTMLTable(data['data']));
@@ -20,28 +20,29 @@ addBtn.onclick = function (){
 function loadHTMLTable(data) {
     const table = document.querySelector('table tbody');
     if (data.length === 0) {
-        table.innerHTML = "<tr><td class='no-data' colspan='6'>No Data</td></tr>";
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
         return;
     }
 
     let tableHtml = "";
-
-    data.forEach(function ({Rollno,Name,no_attended,total_no}) {
+    
+    data.forEach(function ({rollno,name,present,total}) {
         tableHtml += "<tr>";
-        tableHtml += `<td>${Rollno}</td>`;
-        tableHtml += `<td>${Name}</td>`;
-        tableHtml += `<td>${no_attended}</td>`;
-        tableHtml += `<td>${total_no}</td>`;
+        tableHtml += `<td>${rollno}</td>`;
+        tableHtml += `<td>${name}</td>`;
+        tableHtml += `<td>${present}</td>`;
+        tableHtml += `<td>${total}</td>`;
         var pn;
-        if(total_no==0){
+        if(total==0){
             pn=0.00;
         }
         else{
-        var p=no_attended/total_no*100;
+        var p=present/total*100;
         pn = p.toFixed(2);
         }
         tableHtml += `<td>${pn}</td>`;
         tableHtml += "</tr>";
+        
     });
 
     table.innerHTML = tableHtml;
